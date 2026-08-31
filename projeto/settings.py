58648de0726a0feafe34dcdf689edb04db9c6ae9
Geova20 <geovajunior1@gmail.com>
2026-08-31 01:17:57 -0300

    Configura variáveis de ambiente

--- projeto/settings.py
+++ projeto/settings.py
@@ -13,10 +13,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 load_dotenv(BASE_DIR / '.env')
 
@@ -25,10 +29,10 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u-wja_ta44-=xxilkbt6=u%6np@iotxrto9h2_3q@x2wgy-bhv'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
